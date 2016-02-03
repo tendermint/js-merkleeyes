92b49a39a278113eeb485de2d6858c88df984f1d
Jae Kwon <jae@tendermint.com>
2016-02-02 19:39:26 -0800

    js-tmsp takes addr, not socket

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var tmsp = require("js-tmsp");
 var wire = require("js-wire");
 
-function Client(conn) {
-  this.tmspCli = new tmsp.Client(conn);
+function Client(addr) {
+  this.tmspCli = new tmsp.Client(addr);
 }
 
 Client.prototype.get = function(key, cb) {
@@ -14,27 +14,35 @@ Client.prototype.get = function(key, cb) {
     var value = r.readByteArray();
     cb(value);
   });
+  this.tmspCli.flush();
 }
 
-Client.prototype.set = function(key, value) {
+Client.prototype.set = function(key, value, cb) {
   var w = new wire.Writer();
   w.writeByte(0x01); // "Set"
   w.writeByteArray(new Buffer(key));
   w.writeByteArray(new Buffer(value));
-  this.tmspCli.appendTx(w.getBuffer());
+  this.tmspCli.appendTx(w.getBuffer(), function(res) {
+    cb();
+  });
+  this.tmspCli.flush();
 }
 
-Client.prototype.rem = function(key) {
+Client.prototype.rem = function(key, cb) {
   var w = new wire.Writer();
   w.writeByte(0x02); // "Rem"
   w.writeByteArray(new Buffer(key));
-  this.tmspCli.appendTx(w.getBuffer());
+  this.tmspCli.appendTx(w.getBuffer(), function(res) {
+    cb();
+  });
+  this.tmspCli.flush();
 }
 
 Client.prototype.getHash = function(cb) {
   this.tmspCli.getHash(function(res) {
     cb(res.data.toBuffer());
   });
+  this.tmspCli.flush();
 }
 
 module.exports = {
